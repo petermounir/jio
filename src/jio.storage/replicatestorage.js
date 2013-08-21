9bb3f7e65e3f8f58ca38f0bc4bbc8429cca7ef3c
Tristan Cavelier <tristan.cavelier@tiolive.com>
2013-08-21 13:45:16 +0200

    replicatestorage license added

--- src/jio.storage/replicatestorage.js
+++ src/jio.storage/replicatestorage.js
@@ -1,3 +1,21 @@
+/*
+ * JIO extension for data duplication and synchronization.
+ * Copyright (C) 2013  Nexedi SA
+ *
+ *   This library is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /*jslint indent: 2, maxlen: 80, nomen: true, regexp: true */
 /*global define, jIO */
 
